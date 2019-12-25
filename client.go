97cdcfa84dd3c9d5a835b49c7b7d6a828ac2702d
muchiko <shoji0521@gmail.com>
2019-12-25 12:06:29 +0900

    s/c

---
+++ client.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/muchiko/go_grpc_mqtt/client"
+)
+
+func main() {
+	client.Run()
+}
---
+++ server.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/muchiko/go_grpc_mqtt/server"
+)
+
+func main() {
+	server.Run()
+}
